7781b59835927a16c87d6f402934ffd2fb141d31
carlos-jmh <75096034+carlos-jmh@users.noreply.github.com>
2023-02-23 11:39:20 -0500

    Testing CI/CD

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "helloWorldApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "function": {
     "sayHelloWorld": {
       "Arn": "string",
