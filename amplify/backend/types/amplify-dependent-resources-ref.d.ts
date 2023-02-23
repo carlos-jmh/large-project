c4a438e7a8bcdcd0651426bf222f400b6d04eea1
carlos-jmh <75096034+carlos-jmh@users.noreply.github.com>
2023-02-22 21:12:32 -0500

    More boilerplate things

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,22 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "helloWorldApi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "largeproject": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "function": {
     "sayHelloWorld": {
       "Arn": "string",
