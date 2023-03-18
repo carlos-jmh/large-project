a146b52ad90295194774bef854199a00f5fabefe
carlos-jmh <75096034+carlos-jmh@users.noreply.github.com>
2023-03-18 19:05:58 -0400

    Adding Cognito Auth to Web

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+Amplify.configure(awsConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
