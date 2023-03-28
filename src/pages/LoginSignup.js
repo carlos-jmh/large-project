044d1e80ba25c5178ab7b5d5245f69662f48671d
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-28 17:12:44 -0400

    Add basic footer. Make same color as navbar. Add to landing and login pages.

--- src/pages/LoginSignup.js
+++ src/pages/LoginSignup.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from '../components/auth/Auth'
 import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 
 const LoginSignup = (props) => {
   return (
     <div className='wrapper'>
         <Navbar/>
         <Auth auth={props.auth}/>
+        <Footer/>
     </div>
   );
 }
