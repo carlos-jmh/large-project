6f1c9b1a7ff62c225dc98d6c4c5c64c037cdd2bc
Sophia Hunt <sophiahunt2001@icloud.com>
2023-03-28 19:54:10 -0400

    Created component for the body of the landing page, as well as adding Google fonts to the project

---
+++ src/components/landingbody/LandingBody.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './landingbody.css'
+
+function LandingBody()
+{
+return(
+    <div className = "center-text">
+        <span className = "font-bold">
+            Welcome to HouseHold
+        </span>
+    </div>
+)
+}
+
+export default LandingBody
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar';
+import LandingBody from '../components/landingbody/LandingBody'
 
 function Landing() {
   return (
     <div>
         <Navbar/>
-        <h2>Landing</h2>
+        <LandingBody/>
     </div>
   )
 }
