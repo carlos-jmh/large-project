690c553fa584deafcd4cacd92ba8ac4ec5afb983
Sophia Hunt <sophiahunt2001@icloud.com>
2023-03-28 18:21:48 -0400

    Added HouseHold logo to Navbar, as well as styling for the image in navbar.css

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './navbar.css'
+import LogoSquare from '../../images/logo_square.png';
+import LogoRound from '../../images/logo_round.png';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
         {/* Replace with image logo once we have that downloaded and ready*/}
         <a className="navbar-brand" href="/">
-            <img src="./favicon.ico" 
-            alt="household brand logo here, house held up by two hands">
-            </img>
+            <img src={LogoRound} 
+            alt="household brand logo here, house held up by two hands"/>
             <span></span>
         </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
