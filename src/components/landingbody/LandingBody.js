f5bfd641aed899b5597c17a1751dde1f521f2023
Sophia Hunt <sophiahunt2001@icloud.com>
2023-03-28 20:24:10 -0400

    Added start now button to the landing body component. Button on click redirects to signup page.

--- src/components/landingbody/LandingBody.js
+++ src/components/landingbody/LandingBody.js
@@ -5,8 +5,21 @@ function LandingBody()
 {
 return(
     <div className = "center-text">
-        <span className = "font-bold">
-            Welcome to HouseHold
+        <div className = "main-title">
+            <span className = "font-bold">
+                Welcome to HouseHold
+            </span>
+        </div>
+        <div className = "main-body">
+            <span className = "font-main">
+                Keep your home clean, organized,<br /> and on the same page with the #1 house manager in the world!
+            </span>
+        </div>
+        <br />
+        <span className = "font-main">
+            <a href="/signup" target="_blank">
+                <button> Start now for free! </button>
+            </a>
         </span>
     </div>
 )
