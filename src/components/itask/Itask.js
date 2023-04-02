44411510520128b7ed07661166a8030cf649ec66
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-02 14:33:33 -0400

    Create individual tasks placed inside of task

---
+++ src/components/itask/Itask.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Itask = (props) => {
+  return (
+    <div className="iTask">
+      <input type="checkbox" id="task1" value="task1"/>
+      <label for="task1">Place prop here</label>
+    </div>
+  )
+}
+
+export default Itask
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import './task.css'
 
-const Task = () => {
+import Itask from '../../components/itask/Itask';
+
+// Take in props:
+// Household name, array of tasks [task1, task2, task3]
+
+// Use map to create Itasks passing in value of task
+const Task = (props) => {
   return (
     <div className="task">
         <h2>Household Name</h2>
         <div className="taskList">
-            {/* Could make iTask a component and when we map, pass task info as prop */}
-            <div className="iTask">
-                <input type="checkbox" id="task1" value="task1"/>
-                <label for="task1">Take out garbage</label>
-            </div>
-            <div className="iTask">
-                <input type="checkbox" id="task1" value="task1"/>
-                <label for="task1">Buy Cleaning Supplies</label>
-            </div>
-            <div className="iTask">
-                <input type="checkbox" id="task1" value="task1"/>
-                <label for="task1">Vacuum</label>
-            </div>
+            <Itask/>
+            <Itask/>
+            <Itask/>
         </div>
     </div>
   )
