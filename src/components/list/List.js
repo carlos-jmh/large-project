b748bc4f1444679075f33d9af277d04ec79645ed
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-03 13:15:46 -0400

    Add list functionality.

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,43 +1,52 @@
-import React from 'react'
-import {useCollapse} from 'react-collapsed';
+import React, {useState} from 'react'
 import ListItem from '../listItem/ListItem';
+import * as Icon from 'react-bootstrap-icons';
+import './list.css'
 
 function List() {
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
-return (
-  <div className="collapsible">
-      <div className="header" {...getToggleProps()}>
-          {isExpanded ? 'Grocery List' : '> Grocery List'}
-      </div>
-      <div {...getCollapseProps()}>
-          <div className="content">
-              <ListItem /> <br/><br/>
-              Click again to hide...
+  const [open, setOpen] = useState(false);
+
+  const showOrHide = () => {
+    setOpen(!open);
+  }
+
+  return (
+    <div className="arrayList">
+        <div className="section">
+          <h6 className="sectionHeader">
+            {open ? <Icon.CaretRight onClick={showOrHide}/> : <Icon.CaretDown onClick={showOrHide}/>}
+            Grocery List
+          </h6>
+          <hr className="listLine"></hr>
+          <div className="items">
+            {/* Place map of listitems after ? */}
+            { open ? <><ListItem/><ListItem/> </>: <></> }
           </div>
-      </div>
-  </div>
+        </div>
+    </div>
   );
 }
 
 export default List
 
-// Understand how this list can be altered/deleted
-/*const List = (props) => {
-  return (
-    <div>
-        <div className="heading">
-            <p>List Name</p>
-            <p>Num Items</p>
-        </div>
+// // Understand how this list can be altered/deleted
+// const List = (props) => {
+//   return (
+//     <div>
+//         <div className="heading">
+//             <p>List Name</p>
+//             <p>Num Items</p>
+//         </div>
 
-        Understand what actions are allowed on these items
-        Make this collapsible
-        <div className="itemList">
-            <p>Bannana</p>
-            <p>Strawberry</p>
-            <p>Apple</p>
-        </div>
-    </div>
-  )
-}
-*/
+//         {/* Understand what actions are allowed on these items*/}
+//         {/* Make this collapsible */}
+//         <div className="itemList">
+//             <p>Bannana</p>
+//             <p>Strawberry</p>
+//             <p>Watermelon</p>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default List
--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -3,11 +3,28 @@ import React from 'react'
 function strike() {
     document.getElementById("item1").innerHTML = <s>Apple</s>
 }
+
+// function ListItem() {
+// return (
+//   <div>
+//     <p id = "item1" onClick = {() => strike()}>Apple</p>
+//   </div>
+//   );
+// }
+
 function ListItem() {
-return (
-  <div>
-    <p id = "item1" onClick = {() => strike()}>Apple</p>
-  </div>
+  return (
+    <div className="task">
+        <div className="iTask">
+          {/* onClick change database data to complete and refresh*/}
+          <input type="checkbox" id="task1" value="task1"/>
+          <div class="taskInfo">
+            <label htmlFor="task1">Place task name here</label>
+            <p>Place event date, blank if no event</p>
+          </div>
+        </div>
+        <hr></hr>
+    </div>
   );
 }
 
