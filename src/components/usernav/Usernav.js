d7909b9862ea5d44b6d1122f44718ee7103c5477
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-03 12:20:31 -0400

    Navbar icon size update

--- src/components/usernav/Usernav.js
+++ src/components/usernav/Usernav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './usernav.css'
 import * as Icon from 'react-bootstrap-icons'
 
 const Usernav = () => {
@@ -7,16 +8,16 @@ const Usernav = () => {
             {/* Replace with image logo once we have that downloaded and ready*/}
             {/* Place other icons here */}
             <div className="navbar-nav">
-                <a className="nav-item nav-link active " href="/"><Icon.House/></a>
-                <a className="nav-item nav-link active" href="/"><Icon.ThreeDotsVertical/></a>
+                <a className="nav-item nav-link size" href="/"><Icon.House size="27px"/></a>
+                <a className="nav-item nav-link size" href="/"><Icon.ThreeDotsVertical size="27px"/></a>
             </div>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse flex-grow-0" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="/"><Icon.PlusLg/></a>
-              <a className="nav-item nav-link active" href="/"><Icon.PersonCircle/></a>
+              <a className="nav-item nav-link" href="/"><Icon.PlusLg size="27px"/></a>
+              <a className="nav-item nav-link" href="/"><Icon.PersonCircle size="27px"/></a>
             </div>
           </div>
         </nav>
