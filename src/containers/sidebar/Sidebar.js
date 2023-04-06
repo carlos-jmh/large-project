05957ee056c6bf936bb6d4fe278eec09e19f0883
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-06 16:06:40 -0400

    beginner funbctions.

--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -6,6 +6,17 @@ import * as Icon from 'react-bootstrap-icons';
 
 // Vertically list component of householdInfo
 const Sidebar = () => {
+
+  // Set button to active class (background color more grey)
+  // Pass value to dashboard that allows inbox value to be changed.
+  function loadInbox() {
+
+  }
+
+  function loadUpcoming() {
+
+  }
+
   return (
     <div class="sidebarButtons">
         {/* Get list of all households, as well as pertinent information regarding them.
@@ -13,11 +24,11 @@ const Sidebar = () => {
             Goes in component householdInfo pass household name, tasks due today"
         */}
         <div class="views">
-          <button>
+          <button className="in" onClick={loadInbox()}>
             <Icon.InboxFill/>
             <h6>Inbox</h6>
           </button>
-          <button>
+          <button className="upcoming" onClick={loadUpcoming()}>
             <Icon.Calendar3/>
             <h6>Upcoming</h6>
           </button>
