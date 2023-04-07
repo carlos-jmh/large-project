6005f237aa448cd64a00a48fc055a4847842a478
gmartin02 <gabriel.martin1202@gmail.com>
2023-04-06 20:20:37 -0400

    toggle sidebar implemented for new ui

--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Householdinfo from '../../components/householdinfo/Householdinfo'
 import './sidebar.css'
+import * as Icon from 'react-bootstrap-icons'
 
-import * as Icon from 'react-bootstrap-icons';
 
 // Vertically list component of householdInfo
 const Sidebar = () => {
-
+  const [style, setStyle] = useState("container");
+  const [views, setViews] = useState("views");
+  const [isOpen, setIsOpen] = useState(true);
   // Set button to active class (background color more grey)
   // Pass value to dashboard that allows inbox value to be changed.
   function loadInbox() {
@@ -17,13 +19,28 @@ const Sidebar = () => {
 
   }
 
+  function toggle() {
+    setIsOpen((isOpen) => !isOpen); 
+
+    if(isOpen) {
+      setStyle("container");
+    } else {
+      setStyle("container-close");
+    }
+    
+  }
+
   return (
-    <div class="sidebarButtons">
+  
+
+  <div class = {style}>
+      <button class = "closeButton" onClick={() => toggle()}><Icon.ThreeDotsVertical size="27px" color = "rgb(150, 150, 150)"/></button>
+      {isOpen && <div>
         {/* Get list of all households, as well as pertinent information regarding them.
             Create map of all the information.
             Goes in component householdInfo pass household name, tasks due today"
         */}
-        <div class="views">
+        <div class={views}>
           <button className="in" onClick={loadInbox()}>
             <Icon.InboxFill/>
             <h6>Inbox</h6>
@@ -38,8 +55,55 @@ const Sidebar = () => {
           <Householdinfo name="Dorm"/>
           <Householdinfo name="Apartment"/>
         </div>
+    </div>}
     </div>
   )
 }
 
 export default Sidebar
+
+
+
+/*
+
+import React, { useState } from 'react'
+import Householdinfo from '../../components/householdinfo/Householdinfo'
+import './sidebar.css'
+import * as Icon from 'react-bootstrap-icons'
+
+// Vertically list component of householdInfo
+const Sidebar = () => {
+  const [style, setStyle] = useState("container");
+  const [sidebar, setSidebar] = useState("sidebar");
+  const [isOpen, setIsOpen] = useState(true);
+
+  function toggle() {
+    setIsOpen((isOpen) => !isOpen); 
+
+    if(isOpen) {
+      setStyle("container");
+      setSidebar("sidebar");
+    } else {
+      setStyle("container-close");
+      setSidebar("sidebar-close");
+    }
+    
+  }
+
+  
+  return (
+    <div class = {style}>
+      <button class = "closeButton" onClick={() => toggle()}><Icon.ThreeDotsVertical size="27px" color = "rgb(150, 150, 150)"/></button>
+      {isOpen && <div className= {sidebar}>
+          {/* Get list of all households, as well as pertinent information regarding them.
+              Create map of all the information.
+              Goes in component householdInfo pass household name, tasks due today"
+         
+          <Householdinfo name="Dorm"/>
+          <Householdinfo name="Apartment"/>
+      </div>}
+    </div>
+  );
+}
+
+export default Sidebar*/
