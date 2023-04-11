e54d539cbd0504e850114add6a7db7a8f8142ec5
gmartin02 <gabriel.martin1202@gmail.com>
2023-04-11 12:35:05 -0400

    added household join button

--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -41,7 +41,7 @@ const Sidebar = () => {
         </div>
 
         <div id = "addNewHousehold">
-
+          <button class = "addbutton"><Icon.HouseAdd size="30px" /></button>
         </div>
 
         <div id = "joinHousehold">
@@ -52,49 +52,3 @@ const Sidebar = () => {
 }
 
 export default Sidebar
-
-
-
-/*
-
-import React, { useState } from 'react'
-import Householdinfo from '../../components/householdinfo/Householdinfo'
-import './sidebar.css'
-import * as Icon from 'react-bootstrap-icons'
-
-// Vertically list component of householdInfo
-const Sidebar = () => {
-  const [style, setStyle] = useState("container");
-  const [sidebar, setSidebar] = useState("sidebar");
-  const [isOpen, setIsOpen] = useState(true);
-
-  function toggle() {
-    setIsOpen((isOpen) => !isOpen); 
-
-    if(isOpen) {
-      setStyle("container");
-      setSidebar("sidebar");
-    } else {
-      setStyle("container-close");
-      setSidebar("sidebar-close");
-    }
-    
-  }
-
-  
-  return (
-    <div class = {style}>
-      <button class = "closeButton" onClick={() => toggle()}><Icon.ThreeDotsVertical size="27px" color = "rgb(150, 150, 150)"/></button>
-      {isOpen && <div className= {sidebar}>
-          {/* Get list of all households, as well as pertinent information regarding them.
-              Create map of all the information.
-              Goes in component householdInfo pass household name, tasks due today"
-         
-          <Householdinfo name="Dorm"/>
-          <Householdinfo name="Apartment"/>
-      </div>}
-    </div>
-  );
-}
-
-export default Sidebar*/
