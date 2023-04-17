472fb5d467f03de9d21de676bb9999aa47aef615
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-17 14:19:37 -0400

    Comment household info test.

--- src/components/__tests__/householdinfo.test.js
+++ src/components/__tests__/householdinfo.test.js
@@ -2,12 +2,12 @@ import { render, screen } from "@testing-library/react";
 import Householdinfo from '../householdinfo/Householdinfo';
 
 test('verify household info displays correct information', () => {
-    // Render household info with passed info.
+    // Make sure value is of correct data.
     let name = "dorm";
     render(<Householdinfo name={name}/>);
     expect(screen.getByRole('button').id).toBe(name);
     
-    
+    // Make sure displays correct info.
     const container = document.getElementsByTagName('span')[0];
     expect(container.textContent).toBe(name);
 
