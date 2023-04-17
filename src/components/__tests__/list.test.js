7f3ecbeb0b311b7591f753456b52f00918da6c18
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-17 15:15:42 -0400

    Update list test.

--- src/components/__tests__/list.test.js
+++ src/components/__tests__/list.test.js
@@ -1,7 +1,7 @@
 import { React, useState } from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import List from '../list/List';
-import ListItem from '../listItem/ListItem';
+import * as Icon from 'react-bootstrap-icons';
 import data from '../../containers/middle/data.json';
 
 let toDoList = data;
@@ -20,13 +20,14 @@ const handleToggle = (id) => {
 
 // Verify items have correct ammount of list items.
 test('Correct amount of list items', () => {
-    render(<List toDoList={toDoList} handdleToggle={handleToggle}/>)
+    const { list } = render(<List toDoList={toDoList} handdleToggle={handleToggle}/>)
 
     // On load, list should be closed, and have no children.
     const container = document.getElementsByClassName('items');
-    expect(container.children).toBe('undefined');
+    expect(container.children).toBe(undefined);
 
     // On open, list should update children.
     // How can I alter setOpen in my rendered list?
+    // fireEvent.click(list(<Icon.CaretRight/>))
     // expect(container.children.length).toBe(Object.keys(data).length);
 });
