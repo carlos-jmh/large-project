4fd3078fee77daa04c4d49efe9b64aef08923cbf
gmartin02 <gabriel.martin1202@gmail.com>
2023-04-18 14:24:03 -0400

    altered the fields to only show necessary ones per component

--- src/components/add/Add.js
+++ src/components/add/Add.js
@@ -35,17 +35,32 @@ const Add = ({addTask, name}) => {
       </div>
     )
   else 
-    return (
-      <form onSubmit={handleSubmit} className="addingTask">
-        <input type="text" value={userInput} onChange={handleChange} class="form-control" id="name" placeholder="Task/Item Name"/>
-        <input type="datetime-local" value={userDate} class="form-control" id="date"/>
-        <hr></hr>
-        <div className="buttons">
-          <button class="btn btn-danger" onClick={changeAdd}>close</button>
-          <button class="btn" id="purple">add</button>
-        </div>
-      </form>
-    )
+    if(name == "Event") {
+      return (
+        <form onSubmit={handleSubmit} className="addingTask">
+          <input type="text" value={userInput} onChange={handleChange} class="form-control" id="name" placeholder="Event Name"/>
+          <input type="text" value={userInput} onChange={handleChange} class="form-control" id="name" placeholder="Event Description"/>
+
+          <input type="datetime-local" value={userDate} class="form-control" id="date"/>
+          <hr></hr>
+          <div className="buttons">
+            <button class="btn btn-danger" onClick={changeAdd}>close</button>
+            <button class="btn" id="purple">add</button>
+          </div>
+        </form>
+      )
+    } else {
+      return (
+        <form onSubmit={handleSubmit} className="addingTask">
+          <input type="text" value={userInput} onChange={handleChange} class="form-control" id="name" placeholder="Task/Item Name"/>
+          <hr></hr>
+          <div className="buttons">
+            <button class="btn btn-danger" onClick={changeAdd}>close</button>
+            <button class="btn" id="purple">add</button>
+          </div>
+        </form>
+      )
+    }
 }
 
 export default Add
