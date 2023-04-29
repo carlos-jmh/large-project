4e34fea878ff662f72a637aa91d2200e49057fb3
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-29 19:04:50 -0400

    Fix edit list display.

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -7,6 +7,7 @@ import { deleteExistingList, editExistingList } from '../../api/mutating';
 
 const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
   const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState(false);
   
   const newName = useRef('');
   // console.log(listItems);
@@ -17,9 +18,8 @@ const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
 
   // Function to open editList.
   function showList() {
-
     // Show edit form.
-    document.getElementById(list.id).style.display = "block";
+    setEdit(true);
   }
 
   const deleteList = async(e) => {
@@ -27,7 +27,7 @@ const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
     // Call API to delete list. Requires listId. 
     
     // Refresh the page.
-    document.getElementById(list.id).style.display = "none";
+    setEdit(false);
 
     const deleteList = await deleteExistingList(list.id);
 
@@ -43,6 +43,7 @@ const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
   }
 
   const doneList = async(e) => {
+    console.log(newName.current.value);
 
     // If the name was changed.
     if (list.title !== newName.current.value)
@@ -51,8 +52,6 @@ const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
       let copyList = list;
       copyList.title = newName.current.value;
       console.log(copyList);
-
-      document.getElementById(list.id).style.display = "none";
 
       // Call API to updateList
       const updatedList = await editExistingList(copyList);
@@ -66,44 +65,53 @@ const List = ({list, name, setState, listItems, listIndex, handleToggle}) => {
           return newListData;
         })
       }
-    }
-    document.getElementById(list.id).style.display = "none";   
 
-    // After done, refresh page?, set display to none.    
+      setEdit(false);
+    }
+    
+    setEdit(false); 
   }
 
   return (
-    <div className="arrayList">
-      <div className="section">
-          <h6 className="sectionHeader">
-            <div>
-              {open ? <Icon.CaretDown onClick={showOrHide}/> : <Icon.CaretRight onClick={showOrHide}/>}
-              {name}
-            </div>
-            <Icon.ThreeDots onClick={showList}/>
-          </h6>
-          <hr className="listLine"></hr>
-          
-          <div className="items">
-            {/* Place map of listitems after ? */}
-            {/* Items: title, id, houseHoldId, description, completed */}
-            {open ? listItems.map((item, index) => {
+    <>
+    {
+      !edit ? 
+      (
+        <div className="arrayList">
+          <div className="section">
+            <h6 className="sectionHeader">
+              <div>
+                {open ? <Icon.CaretDown onClick={showOrHide}/> : <Icon.CaretRight onClick={showOrHide}/>}
+                {name}
+              </div>
+              <Icon.ThreeDots onClick={showList}/>
+            </h6>
+            <hr className="listLine"></hr>
+              
+            <div className="items">
+              {/* Place map of listitems after ? */}
+              {/* Items: title, id, houseHoldId, description, completed */}
+              {open ? listItems.map((item, index) => {
                 return <ListItem key={index} item={item} listIndex={listIndex} itemIndex={index} handleToggle={handleToggle} lname={name}/>
-            }) : <></>}
-          </div>
-
-          {/* Contain list name, available for edit, delete and done button */}
-          <div id={list.id} className="editList" style={{"display":"none"}}>
-            <label htmlFor="listName">List Name:</label>
-            <input type="text" className="form-control" id="listName" placeholder={name} ref={newName}></input>
-            <div className="editListBut">
-              <button onClick={deleteList}>Delete</button>
-              <button onClick={doneList}>Done</button>
+              }) : <></>}
             </div>
           </div>
-      </div>
-    </div>
-    );
+        </div>
+      )
+      :
+      (
+        <div id={list.id} className="editList">
+          <label htmlFor="listName">List Name:</label>
+          <input type="text" className="form-control" id="listName" placeholder={name} ref={newName}></input>
+          <div className="editListBut">
+            <button onClick={deleteList}>Delete</button>
+            <button onClick={doneList}>Done</button>
+          </div>
+        </div>
+      )
+    }
+    </>
+  );
 };
 
 export default List;
