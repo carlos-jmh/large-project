af3f7998ca3f707d040cbb31a1d858a265f97121
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-29 17:33:08 -0400

    Editing listItems.

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as Icon from 'react-bootstrap-icons';
  
 const ListItem = ({item, listIndex, itemIndex, handleToggle, lname}) => {
+  
+  const [shown, setShown] = useState(false);
+
+
   const handleClick = (e) => {
       e.preventDefault();
       item.completed = !item.completed;
       handleToggle(item, listIndex, itemIndex);
   }
 
+  const editListItem = async () => {
+    let p = document.getElementById(item.title);
+    console.log(p);
+    p.contentEditable = true;
+  }
+
    return (
-      <div value={item.id} onClick={handleClick} className={item.completed ? "todo strike" : "todo"}>
-        {item.title}
+      <div id={item.id} value={item.id}  
+      className={item.completed ? "todo strike" : "todo"}
+      onMouseEnter={() => setShown(true)}
+      onMouseLeave={() => setShown(false)}>
+        <p id={item.title} onClick={handleClick}>{item.title}</p>
+        { shown && (<Icon.ThreeDots className="threedots" onClick={editListItem}/>) }
+        
       </div>
    );
 };
