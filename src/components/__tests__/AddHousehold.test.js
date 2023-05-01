1d0ead5a6bdfd2d1877533f2e3444600c423b505
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-30 21:12:25 -0400

    Editing some unit testing.

--- src/components/__tests__/AddHousehold.test.js
+++ src/components/__tests__/AddHousehold.test.js
@@ -9,16 +9,25 @@ const addNewHousehold = (houseHold) => {
     houseHolds = [...houseHolds, houseHold];
   }
 
+
+test('Verify dark theme works', () => {
+  const { container } = render(<AddHousehold addNewHousehold={addNewHousehold}/>);
+
+  //screen.debug();
+  expect(container.getElementsByClassName('addHouse-dark')[0]).toBeInTheDocument();
+})
 test('Verify click changes form', () => {
-    // Render component.
-    const { container } = render(<AddHousehold addNewHousehold={addNewHousehold}/>)
+  // Render component.
+  const { container } = render(<AddHousehold addNewHousehold={addNewHousehold} theme="light"/>)
 
 
-    // Check initial state. 
-    expect(container.getElementsByClassName('addHouse')[0]).toBeInTheDocument();
+  // Check initial state.
+  //screen.debug();
+  expect(container.getElementsByClassName('addHouse')[0]).toBeInTheDocument();
 
-    // Check when button clicks form changes.
-    fireEvent.click(container.getElementsByClassName('addHouse')[0]);
+  // Check when button clicks form changes.
+  fireEvent.click(container.getElementsByClassName('addHouse')[0]);
+  //screen.debug();
 
-    expect(container.getElementsByClassName('btn-danger')[0].textContent).toContain("close");
+  expect(container.getElementsByClassName('btn-danger')[0].textContent).toContain("close");
 })
--- src/components/__tests__/householdinfo.test.js
+++ src/components/__tests__/householdinfo.test.js
@@ -1,14 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import Householdinfo from '../householdinfo/Householdinfo';
+import { houseHoldMembersByUserProfileId } from "../../graphql/queries";
 
 test('verify household info displays correct information', () => {
-    // Make sure value is of correct data.
-    let name = "dorm";
-    render(<Householdinfo name={name}/>);
-    expect(screen.getByRole('button').id).toBe(name);
+
+    let houseHold = {
+        calendarId: "002307f8-c68c-49f3-b296-6a61d16b0f5e",
+        chatRoomId: "4047c772-ed21-4e94-a581-19de353ca618",
+        createdAt: "2023-04-23T16:49:25.903Z",
+        houseHoldCalendarId: null,
+        houseHoldChatRoomId: null,
+        id: "454fc5ce-b9ae-4491-9e09-751fbc3edcd9",
+        name: "gabe's house 5",
+        owners: ['8202e7fb-b55c-4347-930e-93d34bc791ef'],
+        updatedAt: "2023-04-30T14:17:28.089Z",
+        _deleted: null,
+        _lastChangedAt: 1682864248121,
+        _version: 12
+    }
+
+    render(<Householdinfo houseHold={houseHold} houseHoldClickHandler={() => {}} theme="light"/>);
+    screen.debug();
+    expect(screen.getByRole('button').id).toBe(houseHold.id);
     
     // Make sure displays correct info.
     const container = document.getElementsByTagName('span')[0];
-    expect(container.textContent).toBe(name);
-
+    expect(container.textContent).toBe(houseHold.name);
 })
