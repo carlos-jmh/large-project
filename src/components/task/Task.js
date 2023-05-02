f6847e2489df827a958e317849c31840e8ecce93
zacharyblum2 <blum.zachary1@gmail.com>
2023-05-02 00:00:04 -0400

    Edit task and delete works.

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -138,10 +138,6 @@ const Task = ({task, eventHandlerData, taskIndex, handleCheck, type, handleDelet
     );
 
     let updated = await fetchEventHandlerById(update);
-<<<<<<< HEAD
-=======
-    console.log(updated);
->>>>>>> f90d5ba (Edit and delete event working.)
 
     handleEventUpdate();
   }
